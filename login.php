909dc631cf36c1ff916b7a2847596db3ea9aa831
Alexyndrite <160237068+Arkynexus@users.noreply.github.com>
2025-10-20 14:16:32 +0700

    Update login.php

--- login.php
+++ login.php
@@ -38,6 +38,10 @@
             <button class="btn btn-primary" type="submit">Sign in</button>
           </div>
         </form>
+        <p class="text-center text-muted mt-3 mb-0 small">
+            Don’t have an account?
+           <a href="register.php" class="text-decoration-none">Create one</a>
+        </p>
 
         <p class="text-center text-muted mt-3 mb-0 small">Demo only — do not use weak passwords.</p>
       </div>
@@ -46,3 +50,4 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
